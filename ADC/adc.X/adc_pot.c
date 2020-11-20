299b2ce0bb858f7475688d1df1ebfdb4d460b529
fyvivas <fyvivas@gmail.com>
2020-11-20 11:01:09 -0500

    new adc prj

---
+++ ADC/adc.X/adc_pot.c
@@ -0,0 +1,22 @@
+
+#include    <xc.h>
+#include    "fuses.h"
+
+void main(void) {
+    TRISD = 0x00;
+    TRISA = 0xFF;
+    OSCCONbits.IRCF = 0b111; //Configura oscilador interno (FOSC = 8Mhz)
+    OSCCONbits.SCS = 0b10; // Oscilador del sistema = Fosc interno
+    ADCON1bits.PCFG = 0b1110; //  Configura el Puerto como Entrada Analógica.
+    ADCON1bits.VCFG = 0b00; //  Selecciona Voltajes de Referencia (5v-0v).
+    ADCON0bits.CHS = 0b0000; //  Selecciona el Canal Analógico.
+    ADCON2bits.ACQT = 0b010; //  Tiempo de Adquisición 4Tad.
+    ADCON2bits.ADCS = 0b001; //  Tiempo de Conversión Fosc/8.
+    ADCON2bits.ADFM = 0; //  Justificación Izquierda (modo-8bits).
+    ADCON0bits.ADON = 1; //  Habilita el Módulo AD.
+    while (1) {
+        ADCON0bits.GO_DONE = 1; //  Inicia la COnversió AD.
+        while (ADCON0bits.GO_DONE); //  Espera a que termine la conversión AD.
+        LATD = ADRESH; //  Lectura de valor AD.
+    }
+}
---
+++ ADC/adc.X/build/default/production/adc_pot.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/adc_pot.d  \
+ build/default/production/adc_pot.p1:  \
+ adc_pot.c  \
+fuses.h 
